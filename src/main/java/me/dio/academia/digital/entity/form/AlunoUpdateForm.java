5f594305e7818caadadbbecdf5937e64fe937968
Fillpin <fillipearaujoo97@gmail.com>
2023-04-29 11:54:26 -0300

    Projeto de Academia Digital com Spring DTA Java

---
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoUpdateForm.java
@@ -0,0 +1,10 @@
+package me.dio.academia.digital.entity.form;
+
+public class AlunoUpdateForm {
+
+    private String nome;
+
+    private String bairro;
+
+    private String dataDeNascimento;
+}
---
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -0,0 +1,21 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class MatriculaForm {
+
+    @NotNull(message = "Preencha o campo corretamente.")
+    @Positive(message = "O Id do aluno precisa ser positivo.")
+    private Long alunoId;
+
+}
